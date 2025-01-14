3885eebcb5a9e5bba1c49add558af8120a2add00
DBG <danielbernardograset@gmail.com>
2025-01-14 10:43:24 +0100

    creo.env .gitignore i a .env poso contants del projecte. Afegeixo .env a .gitignore

--- APIs/.js/Node.js/cuinaDaniel/src/index.js
+++ APIs/.js/Node.js/cuinaDaniel/src/index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+app.listen(PORT, () => console.log(`Servidor funcionant al port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
